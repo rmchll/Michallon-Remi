91394cce09b786f945b332625f532170e8a9f8ad
Michallon Remi <gitteub@gmail.com>
2022-09-26 17:51:16 +0200

    votre message

--- src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Field.java
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Field.java
@@ -13,141 +13,151 @@ import javafx.scene.paint.Color;
 
 /**
  * Classe gerant le terrain de jeu.
- * 
+ *
  */
 public class Field extends Canvas {
-	
+
 	/** Joueurs */
 	Player [] joueurs = new Player[2];
 	/** Couleurs possibles */
 	String[] colorMap = new String[] {"blue", "green", "orange", "purple", "yellow"};
 	/** Tableau traçant les evenements */
-    ArrayList<String> input = new ArrayList<String>();
-    
+	ArrayList<String> input = new ArrayList<String>();
 
-    final GraphicsContext gc;
-    final int width;
-    final int height;
-    
-    /**
-     * Canvas dans lequel on va dessiner le jeu.
-     * 
-     * @param scene Scene principale du jeu a laquelle on va ajouter notre Canvas
-     * @param w largeur du canvas
-     * @param h hauteur du canvas
-     */
-	public Field(Scene scene, int w, int h) 
+
+	final GraphicsContext gc;
+	final int width;
+	final int height;
+
+	/**
+	 * Canvas dans lequel on va dessiner le jeu.
+	 *
+	 * @param scene Scene principale du jeu a laquelle on va ajouter notre Canvas
+	 * @param w largeur du canvas
+	 * @param h hauteur du canvas
+	 */
+	public Field(Scene scene, int w, int h)
 	{
-		super(w, h); 
+		super(w, h);
 		width = w;
 		height = h;
-		
+
 		/** permet de capturer le focus et donc les evenements clavier et souris */
 		this.setFocusTraversable(true);
-		
-        gc = this.getGraphicsContext2D();
-        
-        /** On initialise le terrain de jeu */
-    	joueurs[0] = new Player(gc, colorMap[0], w/2, h-50, "bottom");
-    	joueurs[0].display();
 
-    	joueurs[1] = new Player(gc, colorMap[1], w/2, 20, "top");
-    	joueurs[1].display();
+		gc = this.getGraphicsContext2D();
+
+		/** On initialise le terrain de jeu */
+		joueurs[0] = new Player(gc, colorMap[0], w/2, h-50, "bottom");
+		joueurs[0].display();
+
+		joueurs[1] = new Player(gc, colorMap[1], w/2, 20, "top");
+		joueurs[1].display();
 
 
-	    /** 
-	     * Event Listener du clavier 
-	     * quand une touche est pressee on la rajoute a la liste d'input
-	     *   
-	     */
-	    this.setOnKeyPressed(
-	    		new EventHandler<KeyEvent>()
-	    	    {
-	    	        public void handle(KeyEvent e)
-	    	        {
-	    	            String code = e.getCode().toString();
-	    	            // only add once... prevent duplicates
-	    	            if ( !input.contains(code) )
-	    	                input.add( code );
-	    	        }
-	    	    });
+		/**
+		 * Event Listener du clavier
+		 * quand une touche est pressee on la rajoute a la liste d'input
+		 *
+		 */
+		this.setOnKeyPressed(
+				new EventHandler<KeyEvent>()
+				{
+					public void handle(KeyEvent e)
+					{
+						String code = e.getCode().toString();
+						// only add once... prevent duplicates
+						if ( !input.contains(code) )
+							input.add( code );
+					}
+				});
 
-	    /** 
-	     * Event Listener du clavier 
-	     * quand une touche est relachee on l'enleve de la liste d'input
-	     *   
-	     */
-	    this.setOnKeyReleased(
-	    	    new EventHandler<KeyEvent>()
-	    	    {
-	    	        public void handle(KeyEvent e)
-	    	        {
-	    	            String code = e.getCode().toString();
-	    	            input.remove( code );
-	    	        }
-	    	    });
-	    
-	    /** 
-	     * 
-	     * Boucle principale du jeu
-	     * 
-	     * handle() est appelee a chaque rafraichissement de frame
-	     * soit environ 60 fois par seconde.
-	     * 
-	     */
-	    new AnimationTimer() 
-	    {
-	        public void handle(long currentNanoTime)
-	        {	 
-	            // On nettoie le canvas a chaque frame
-	            gc.setFill( Color.LIGHTGRAY);
-	            gc.fillRect(0, 0, width, height);
-	        	
-	            // Deplacement et affichage des joueurs
-	        	for (int i = 0; i < joueurs.length; i++) 
-	    	    {
-	        		if (i==0 && input.contains("LEFT"))
-	        		{
-	        			joueurs[i].moveLeft();
-	        		} 
-	        		if (i==0 && input.contains("RIGHT")) 
-	        		{
-	        			joueurs[i].moveRight();	        			
-	        		}
-	        		if (i==0 && input.contains("UP"))
-	        		{
-	        			joueurs[i].turnLeft();
-	        		} 
-	        		if (i==0 && input.contains("DOWN")) 
-	        		{
-	        			joueurs[i].turnRight();	        			
-	        		}
-	        		if (i==1 && input.contains("A"))
-	        		{
-	        			joueurs[i].moveLeft();
-	        		} 
-	        		if (i==1 && input.contains("D")) 
-	        		{
-	        			joueurs[i].moveRight();	        			
-	        		}
-	        		if (i==1 && input.contains("W"))
-	        		{
-	        			joueurs[i].turnLeft();
-	        		} 
-	        		if (i==1 && input.contains("S")) 
-	        		{
-	        			joueurs[i].turnRight();	        			
-	        		}
-	        		if (input.contains("SPACE")){
-	        			joueurs[i].shoot();
+		/**
+		 * Event Listener du clavier
+		 * quand une touche est relachee on l'enleve de la liste d'input
+		 *
+		 */
+		this.setOnKeyReleased(
+				new EventHandler<KeyEvent>()
+				{
+					public void handle(KeyEvent e)
+					{
+						String code = e.getCode().toString();
+						input.remove( code );
+					}
+				});
+
+		/**
+		 *
+		 * Boucle principale du jeu
+		 *
+		 * handle() est appelee a chaque rafraichissement de frame
+		 * soit environ 60 fois par seconde.
+		 *
+		 */
+		new AnimationTimer()
+		{
+			public void handle(long currentNanoTime)
+			{
+				// On nettoie le canvas a chaque frame
+				gc.setFill( Color.LIGHTGRAY);
+				gc.fillRect(0, 0, width, height);
+
+				// Deplacement et affichage des joueurs
+				for (int i = 0; i < joueurs.length; i++)
+				{
+					if (joueurs[i].side=="top" && input.contains("LEFT"))
+					{
+						joueurs[i].moveLeft();
+					}
+					if (joueurs[i].side=="top" && input.contains("RIGHT"))
+					{
+						joueurs[i].moveRight();
+					}
+					if (joueurs[i].side=="top" && input.contains("UP"))
+					{
+						joueurs[i].turnLeft();
+					}
+					if (joueurs[i].side=="top" && input.contains("DOWN"))
+					{
+						joueurs[i].turnRight();
 					}
 
-	        		
-	        		joueurs[i].display();
-	    	    }
-	    	}
-	     }.start(); // On lance la boucle de rafraichissement 
-	     
+					if (joueurs[i].side=="top" && input.contains("ENTER"))
+					{
+						joueurs[i].shoot();
+					}
+
+
+					if (i==1 && input.contains("Q"))
+					{
+						joueurs[i].moveLeft();
+					}
+					if (i==1 && input.contains("D"))
+					{
+						joueurs[i].moveRight();
+					}
+					if (i==1 && input.contains("Z"))
+					{
+						joueurs[i].turnLeft();
+					}
+					if (i==1 && input.contains("S"))
+					{
+						joueurs[i].turnRight();
+					}
+					if (input.contains("SPACE")){
+						joueurs[i].shoot();
+					}
+					if (input.contains("ENTER")){
+						joueurs[i].shoot();
+					}
+
+
+					joueurs[i].display();
+				}
+			}
+		}.start(); // On lance la boucle de rafraichissement
+
 	}
 
 	public Player[] getJoueurs() {
